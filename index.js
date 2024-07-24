d5c60e7b9dea22918b033342e2f81f86347cdfe0
Jessica Lafferty <jessica.a.lafferty@gmail.com>
2024-07-23 20:57:25 -0400

    a bit more JS

--- index.js
+++ index.js
@@ -9,13 +9,17 @@ const eventsList = document.querySelector("#events");
 
 const addEventForm = document.querySelector("#addEvent");
 addEventForm.addEventListener("submit", addEvent);
-
+/**
+ * Sync state with the API and rerender
+ */
 sync function render() {
     await getEvents();
     renderEvents();
   }
   render();
-
+/**
+ * Update state with events from API
+ */
   async function getEvents() {
     try {
       const response = await fetch(API_URL);
@@ -25,3 +29,27 @@ sync function render() {
       console.error(error);
     }
   }
+
+/**
+ * Render events from state
+ */
+function renderEvents() {
+    if (!state.events.length) {
+      eventsListList.innerHTML = "<li>No events.</li>";
+      return;
+    }
+  
+    const eventCards = state.events.map((event) => {
+      const li = document.createElement("li");
+      li.innerHTML = `
+        <h2>${events.name}</h2>
+        <p>${events.description}</p>
+        <p>${events.date}</p>
+        <p>${events.location}</p>
+      `;
+      return li;
+    });
+  
+    eventsListList.replaceChildren(...eventCards);
+  }
+  
